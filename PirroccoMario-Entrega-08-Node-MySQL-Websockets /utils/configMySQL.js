5c9ee7997b42a18b86e2881549cd106addba2f0d
mariopirrocco <91997819+mariopirrocco@users.noreply.github.com>
2022-09-20 14:50:37 -0300

    Entrega 08 - MySQL SQLlite 3 Websockets

---
+++ PirroccoMario-Entrega-08-Node-MySQL-Websockets /utils/configMySQL.js
@@ -0,0 +1,11 @@
+export const configMySQL = {
+  db: {
+    client: 'mysql',
+    connection: {
+      host: '127.0.0.1',
+      user: 'coderhouse',
+      password: 'coderhouse',
+      database: 'products'
+    }
+  }
+}
